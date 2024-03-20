40c1335ce3598508f9d542c3b1b5c425bd28b31b
NhatPhi1804 <kenshiro.pn@gmail.com>
2024-03-20 16:53:55 +0700

    feat(ui): Register

---
+++ Views/RegisterPage.xaml.cs
@@ -0,0 +1,41 @@
+using CinemaManagement.Models;
+using CinemaManagement.ViewModels;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Hosting;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Display.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CinemaManagement.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RegisterPage : Page
+    {
+        public RegisterPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
